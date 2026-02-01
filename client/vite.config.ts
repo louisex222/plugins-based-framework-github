6708d2bf4b5724918fdd3560cb83d0e268ccea81
louisex222 <louisex222@gmail.com>
2026-02-02 00:53:33 +0800

    fix: fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/plugins-based-framework-github/' : '/',
+  base: mode === 'production' ? '/plugins-based-framework-github.io/' : '/',
   plugins: [react()],
   server: {
     port: 3000,
